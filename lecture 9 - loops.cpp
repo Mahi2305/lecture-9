de3de37bbb3c84d646b9936ebfd11329ab65725a
Mahi2305 <114794143+Mahi2305@users.noreply.github.com>
2024-07-02 13:47:10 +0530

    Add files via upload

---
+++ lecture 9 - loops.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+	// looping is something in which a similar action is repeated
+	//using for loop to print 1-100
+	int i;
+	for(i=0;i<=100;i++){
+		cout<<i<<" ";
+	}
+}
